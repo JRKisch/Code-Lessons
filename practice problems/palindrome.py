420d3bd14a9df52b93756601ea76db5eff632a2a
Nicholas Wilson <albireoNox@users.noreply.github.com>
2018-01-06 20:08:04 -0600

    Nick's palindrome implementation

--- practice problems/palindrome.py
+++ practice problems/palindrome.py
@@ -1,8 +1,14 @@
 from englishWords import *
 
-def isPalindrome(word):
-    # TODO Implement me!
-    return word == "palindrome"
+def isPalindrome(word):   
+    start = 0
+    end = len(word) - 1
+    while start < end:
+        if word[start] != word[end]:
+            return False
+        start += 1
+        end -= 1
+    return True
 
 for word in englishWords():
     if isPalindrome(word):
